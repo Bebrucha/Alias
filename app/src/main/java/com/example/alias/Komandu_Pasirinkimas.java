24ce0e7e2b152dc3ac7cecc18337aa3c7aa1b7f1
Bebrucha <99428206+Bebrucha@users.noreply.github.com>
2022-03-01 22:37:35 +0200

    Mygtukas atgal iš komandų pasirinkimo psl.

--- app/src/main/java/com/example/alias/Komandu_Pasirinkimas.java
+++ app/src/main/java/com/example/alias/Komandu_Pasirinkimas.java
@@ -3,6 +3,8 @@ package com.example.alias;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Komandu_Pasirinkimas extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class Komandu_Pasirinkimas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_komandu_pasirinkimas);
+
+        //mygtukas Atgal
+        Button Atgal = findViewById(R.id.button_MainMenu);
+        Atgal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+
+        });
+        //mygtukas Atgal
     }
 }
