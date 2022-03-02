26c5e801f2eb7374ea33f5d1b16d246921f81e21
astadantupasta <79766133+astadantupasta@users.noreply.github.com>
2022-03-02 21:16:09 +0200

    Komandu skaiciaus ivedimas

---
+++ app/src/main/java/com/example/alias/Game.java
@@ -0,0 +1,7 @@
+package com.example.alias;
+
+public class Game {
+    int num_of_teams = Komandu_Pasirinkimas.num_of_teams;
+
+
+}
--- app/src/main/java/com/example/alias/Komandu_Pasirinkimas.java
+++ app/src/main/java/com/example/alias/Komandu_Pasirinkimas.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Komandu_Pasirinkimas extends AppCompatActivity {
-
+    public static int num_of_teams = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,13 +17,30 @@ public class Komandu_Pasirinkimas extends AppCompatActivity {
 
         //mygtukas Atgal
         Button Atgal = findViewById(R.id.button_MainMenu);
-        Atgal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-
-        });
+        Atgal.setOnClickListener(v -> finish());
         //mygtukas Atgal
+
+        EditText num_of_teams_label = findViewById(R.id.num_of_teams);
+        Button next_num_of_teams = findViewById(R.id.button_next_num_of_teams);
+
+        // button next
+        next_num_of_teams.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num_of_teams = Integer.parseInt(num_of_teams_label.getText().toString());
+                TextView validation = findViewById(R.id.num_of_teams_validation_label);
+
+                if(num_of_teams >= 1 && num_of_teams <= 10){
+                    validation.setVisibility(View.INVISIBLE);
+
+                    // let go to another page
+                    // COMPLETE THIS PLACE
+                }
+                else
+                {
+                    validation.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
